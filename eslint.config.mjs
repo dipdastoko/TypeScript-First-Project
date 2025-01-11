c7ba55f1542265dc7c4dc1a501ebcbd9810dab8b
rpamgi <dipdastoko@gmail.com>
2025-01-11 13:23:06 +0600

    prettier setup complete

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,29 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 /** @type {import('eslint').Linter.Config[]} */
 
 export default [
-  { files: ["**/*.{ts}"] },
+  { files: ['**/*.{ts}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   {
     rules: {
-      "no-unused-vars": "error",
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+    },
+    globals: {
+      process: 'readonly',
     },
   },
   {
-    ignores: [".node_modules/*"],
+    ignores: ['.node_modules/*'],
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
 ];
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 const app: Application = express();
 // const port = 3000;
 
@@ -7,10 +7,10 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-// var a = 10;
+// const a = 10;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 console.log(process.cwd());
